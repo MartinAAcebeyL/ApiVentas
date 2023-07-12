642bdf38e08fd5dd517c9ce248039b95d2d0d21e
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-12 15:50:12 -0400

    sels model

--- apps/sels/views.py
+++ apps/sels/views.py
@@ -7,6 +7,17 @@ class Sel(models.Model):
         verbose_name = 'sel'
         verbose_name_plural = 'sels'
 
+    PAYMENT_METHODS = (
+        ('cash', 'Cash'),
+        ('qr', 'Qr'),
+        ('transfer', 'Transfer')
+    )
+
     seller = models.ForeignKey('sellers.Seller', on_delete=models.CASCADE)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
