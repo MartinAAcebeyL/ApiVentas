fa490e373d3b18214b8d4543f649d30ac88d3f13
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-12 15:54:35 -0400

    add product model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    weight = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Category(models.Model):
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
