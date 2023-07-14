3d78678b3048c9f9cd1a642a2b7a8281bf90f943
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-13 23:26:56 -0400

    seeders de products

---
+++ apps/products/management/commands/seed_categories.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from .factory import create_category, create_product, create_stock
+
+
+class Command(BaseCommand):
+    help = 'Genera datos falsos para los modeloes categorias, productos y stock'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--cant', nargs='?', default=10)
+        parser.add_argument('--c', nargs='?', default=0)
+
+    def handle(self, *args, **options):
+        amount = options['cant']
+        category = options['c']
+        if category:
+            create_category(amount//2)
+            
+        create_product(amount)
+        create_stock(amount)
+
+        self.stdout.write(self.style.SUCCESS(
+            'Datos falsos generados correctamente'))
