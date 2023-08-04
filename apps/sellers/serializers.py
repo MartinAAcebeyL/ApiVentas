dfeb5361e9c0f1f2f9c82dcb7f5c49b52e16552a
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-08-03 22:17:20 -0400

    update sales and seller serializer

--- apps/sellers/serializers.py
+++ apps/sellers/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Seller
-from apps.sales.models import SaleDetail
+from apps.sales.models import SaleDetail, Sale
+from apps.products.serializers import ProductSerializer
+from apps.products.models import Product
 
 
 class SellerSerializer(serializers.ModelSerializer):
@@ -10,12 +12,15 @@ class SellerSerializer(serializers.ModelSerializer):
 
 
 class ShowSalesSerializer(serializers.ModelSerializer):
-    sale_aux = serializers.SerializerMethodField()
+    sale_products = serializers.SerializerMethodField()
 
     class Meta:
         model = SaleDetail
-        fields = ['sale_aux', 'sale', 'quantity', 'total', 'unit_price',
-                  'payment_method', 'created_at', 'updated_at']
+        fields = ['sale', 'quantity', 'total', 'unit_price',
+                  'payment_method', 'created_at', 'updated_at', 'sale_products']
 
-    def get_sale_aux(self, obj):
-        return obj.sale.id
+    def get_sale_products(self, obj):
+        sale = Sale.objects.get(id=obj.sale.id)
+        product_serializer = ProductSerializer(
+            Product.objects.get(id=sale.product.first().id))
+        return product_serializer.data
