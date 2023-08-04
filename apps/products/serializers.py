ca290ca18263d772e624cf488e7546e634afe520
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-08-03 22:16:28 -0400

    add products and category serializer

---
+++ apps/products/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Product, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'weight', 'price', 'category']
