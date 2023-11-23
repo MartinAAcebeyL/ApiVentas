404f90ccac9ca2a8b1b6a72219bb2d8c7e51c873
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-11-22 22:57:06 -0400

    fix: add try-except to send_email function and signals

--- apps/sales/signals.py
+++ apps/sales/signals.py
@@ -1,10 +1,11 @@
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
-from django.core.mail import send_mail
-from django.conf import settings
 
-from .models import Sale
 from apps.products.models import Product, Stock
+from apps.utils.send_emails import send_email
+from .models import Sale
+
+import os
 
 
 @receiver(m2m_changed, sender=Sale.product.through)
@@ -14,18 +15,11 @@ def check_minimum_stock(sender, instance, action, **kwargs):
         for product_id in products_ids:
             product = Product.objects.filter(id=product_id).first()
             current_stock = Stock.objects.filter(
-                product=product.id).first()
-            print(dir(current_stock))
+                product=product.id).first().current_quantity
 
-            # minimum_stock = product.stock.through.stock.minimum_stock
-            # print(minimum_stock)
-            if 5 < 6:
-                pass
-
-    # print(product)
-    # if instance.minimum_stock < 5:
-    #     subject = '¡Alerta! Valor numérico menor a 5'
-    #     message = f'El valor numérico actual es {instance.campo_numerico}.'
-    #     from_email = settings.DEFAULT_FROM_EMAIL
-    #     to_email = 'destinatario@example.com'
-    #     send_mail(subject, message, from_email, [to_email])
+            if current_stock <= product.minimum_stock:
+                subject = f'¡Alerta! Ya supero el valor minimo de stock de {product.minimum_stock}'
+                message = f'El producto {product.name} esta en su ultimas {current_stock} unidades y debe ver de pedir mas unidades o no.'
+                from_email = os.getenv('EMAIL_HOST_USER')
+                to_email = ['martinaacbyl2000@gmail.com']
+                send_email(message, from_email, to_email, subject)
--- apps/utils/send_emails.py
+++ apps/utils/send_emails.py
@@ -1,13 +1,17 @@
-from django.core.mail import EmailMessage
+from django.core import mail
 
 
-def send_email(message, from_email, to_emails,
-               subject, html_message=None,
-               is_html_activate=False):
-    email = EmailMessage(subject, message, from_email, to_emails)
+def send_email(message: str, from_email: str, to_emails: list[str],
+               subject: str, html_message: str = None,
+               is_html_activate: bool = False):
+    try:
+        email = mail.EmailMessage(subject, message,
+                                  from_email, to_emails)
 
-    if is_html_activate:
-        email.content_subtype = 'html'
-        email.attach_alternative(html_message, 'text/html')
+        if is_html_activate:
+            email.content_subtype = 'html'
+            email.attach_alternative(html_message, 'text/html')
 
-    email.send()
+        email.send(fail_silently=False)
+    except Exception as e:
+        print("SMTP ERROR: ", e)
